846d9cfea1c0e8003c44a300f319c7e44d1c0fcf
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-10-07 08:21:24 +0000

    feat: initialize Strapi application with essential configurations and dependencies
    - Added package.json for Strapi application setup with version 3.6.8 and necessary plugins.
    - Created robots.txt to manage search engine indexing.
    - Added .gitkeep to uploads directory for maintaining structure.

---
+++ apps/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f5cfc3e5e52499beb0b9640343b403a9'),
+    },
+  },
+});
