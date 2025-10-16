21068fdebf5366486d4015c7b54dc71b5df4b00f
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2025-10-16 12:21:20 +0200

    Update config

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://strapi.traefik.me');
+    cy.visit(`https://${Cypress.env('SERVERNAME')}`, {failOnStatusCode: false});
     cy.screenshot('first-page');
-  })
+  });
 })
